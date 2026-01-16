d3a316aec7f90e59c9322a2a3d52472cf0b3b0f1
artemiyulyanov <artemiyulyanov@gmail.com>
2026-01-16 23:09:06 +0300

    feat: common algorithms added

---
+++ graphs/dfs.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <vector>
+
+std::set<int>* dfs(std::vector<std::vector<int>> &adj_list, int start, std::set<int>* visited = nullptr) {
+    if (visited == nullptr) {
+        visited = new std::set<int>();
+    }
+
+    visited->insert(start);
+
+    for (int neighbor : adj_list[start]) {
+        if (visited->find(neighbor) == visited->end()) {
+            dfs(adj_list, neighbor, visited);
+        }
+    }
+
+    return visited;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::vector<std::vector<int>> adj_list(n+1, std::vector<int>()); // 1-based
+
+    for (int i = 0; i < n - 1; i++) {
+        std::string road_str;
+        getline(std::cin, road_str);
+
+        std::stringstream ss(road_str);
+        int num;
+
+        std::vector<int> road(0);
+
+        while (ss >> num) {
+            road.push_back(num);
+        }
+
+        adj_list[road[0]].push_back(road[1]);
+        adj_list[road[1]].push_back(road[0]);
+    }
+
+    std::set<int>* nodes = dfs(adj_list, 1);
+    auto it = nodes->begin();
+
+    for (int i = 0; it != nodes->end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+
+    return 0;
+}
---
+++ prefix-function.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <ranges>
+#include <vector>
+#include <bits/parse_numbers.h>
+
+std::vector<int> prefixFunction(const std::string &line) {
+    std::vector<int> prefix(static_cast<int>(line.length()), 0);
+
+    for (int i = 1; i < line.length(); i++) {
+        int k = prefix[i - 1];
+
+        while (k > 0 && line[i] != line[k]) {
+            k = prefix[k - 1];
+        }
+
+        if (line[i] == line[k]) {
+            k++;
+        }
+
+        prefix[i] = k;
+    }
+
+    return prefix;
+}
+
+std::vector<int> kmp(const std::string &line, const std::string &pattern) {
+    std::string s = pattern + "#" + line;
+    const std::vector<int> prefix = prefixFunction(s);
+
+    const int pl = static_cast<int>(pattern.length());
+    std::vector<int> result{};
+
+    for (int i = 0; i < line.length(); i++) {
+        if (prefix[i + pl + 1] == pl) {
+            result.push_back(i - pl);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    std::string line;
+    std::string pattern;
+
+    std::getline(std::cin, line);
+    std::cin.ignore();
+    std::getline(std::cin, pattern);
+
+    std::vector<int> result = kmp(line, pattern);
+
+    for (int i : result) {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
